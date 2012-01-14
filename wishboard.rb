22ca60470dde14a64cbdedd40728a32b94412903
Gordon Fontenot <gordon.fontenot@me.com>
2012-01-14 10:11:11 -0500

    Add google analytics

--- wishboard.rb
+++ wishboard.rb
@@ -64,6 +64,21 @@ __END__
   <title><%= @title %></title>
 	<link rel="stylesheet" href="/reset.css" type="text/css" media="all" charset="utf-8">
   <link rel="stylesheet" href="/style.css" type="text/css" media="all" charset="utf-8">
+
+  <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-7976262-7']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
+
 </head>
 <body>
   <div id="banner">
